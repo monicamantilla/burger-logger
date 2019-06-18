af4440d04a69fd0588527efb7cb80e847fc152ac
Monica Mantilla <monicamantilla@macbook-pro-2.home>
2019-06-17 20:34:47 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -6,19 +6,19 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'vltljqtf44k5pv15',
-    password: 'jwpb8hknhp4ipmcu',
-    database: 'zkscqs4mc501zyg2'
+    host: "localhost",
+    user: "root",
+    password: "Larita1*",
+    database: "burger_db"
   })
 }
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  Port: 8000,
-  password: "Larita1*",
-  database: "burger_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   Port: 8000,
+//   password: "Larita1*",
+//   database: "burger_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,48 +1,50 @@
 var express = require("express");
-var burger = require("../models/burger.js");
+var burger = require("../models/burger");
 
 var router = express.Router();
 
-router.get("/", function(req, res){
-    burger.selectAll(function(data){
-        var hbObject = {
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hdbrsObj = {
             burgers: data
         };
-        console.log(hbObject);
-        res.render("index", hbObject);
+        console.log(hdbrsObj);
+        res.render("index", hdbrsObj);
     });
 
-    router.post("/api/burgers", function(req,res) {
+    router.post("/api/burgers", function (req, res) {
         burger.insertOne(
             ["burger_name", "devoured"],
             [req.body.burger_name, req.body.devoured],
-            function(result){
-                res.json({ id: result.insertId})
+            function (result) {
+                res.json({ id: result.insertId })
             }
-         );
+        );
     });
-    router.put("/api/burgers/:id", function (req, res){
+    router.put("/api/burgers/:id", function (req, res) {
         var condition = "id = " + req.params.id;
 
-        console.log("condition", condition);
-        burger.updateOne({ devoured: req.body.devoured}, condition, function(result){
-            if((result, changedRows === 0)) {
+        console.log("condition ", condition);
+        burger.updateOne({ devoured: req.body.devoured }, condition, function (result) {
+            if ((result.changedRows === 0)) {
                 return res.status(404).end();
-            } else{
+            } else {
                 res.status(200).end();
             }
         });
     });
-    router.deleteOne(condition, function(req, res){
+    router.delete(condition, function (req, res) {
         var condition = "id = " + req.params.id;
-        console.log("condition", condition);
 
-        if((result, changedRows === 0)) {
-            return res.status(404).end();
-        } else{
-            res.status(200).end();
-        }
+        console.log("Condition ", condition);
+
+        burger.deleteOne(condition, function (result) {
+            if ((result.changedRows === 0)) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
     });
 });
-
 module.exports = router;
